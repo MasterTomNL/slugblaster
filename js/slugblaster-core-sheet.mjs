9544b6c01dfda90ab16cb88d95b1668595ddd8a5
MasterTomNL <tomvanderlek@gmail.com>
2025-10-19 17:31:52 +0200

    Small factions visual fix

--- js/slugblaster-core-sheet.mjs
+++ js/slugblaster-core-sheet.mjs
@@ -3,9 +3,9 @@ export class SlugblasterCoreSheet extends ActorSheet {
     event.preventDefault();
     let type = $(event.currentTarget).data('type'); // item.type
     let systemType = $(event.currentTarget).data('systemType'); // item.system.type
-    console.log(type + ', ' + systemType);
     let parentId = $(event.currentTarget).data('parentId'); // item.system.parentId
     const item = { name: game.i18n.localize(`SB.new_${systemType}`), type: type, ['system.active']: true, ['system.type']: systemType };
+    if (type == 'faction') item['system.level'] = 0;
     if (parentId) item.parentId = parentId; // assign parentId when it's defined
     await Item.create(item, { parent: this.actor }); // create the item
   }
